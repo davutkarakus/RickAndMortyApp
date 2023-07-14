e5bd639dd225a15e07ac728e2f3672bb4a4fb974
Davut KARAKUS <pikachu_018@hotmail.com>
2023-07-14 13:06:37 +0300

    Retrofit service added

---
+++ app/src/main/java/com/davutkarakus/rickandmortyapp/service/CharactersApi.kt
@@ -0,0 +1,12 @@
+package com.davutkarakus.rickandmortyapp.service
+
+import com.davutkarakus.rickandmortyapp.model.Characters
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface CharactersApi {
+    // https://rickandmortyapi.com/api/character
+
+    @GET("https://rickandmortyapi.com/api/character")
+    fun getAllCharacters() : Single<Characters>
+}
---
+++ app/src/main/java/com/davutkarakus/rickandmortyapp/service/CharactersApiService.kt
@@ -0,0 +1,22 @@
+package com.davutkarakus.rickandmortyapp.service
+
+import com.davutkarakus.rickandmortyapp.model.Characters
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class CharactersApiService {
+    // https://rickandmortyapi.com/api/character
+    private val BASE_URL = "https://rickandmortyapi.com/"
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(CharactersApi::class.java)
+
+    fun getData() : Single<Characters> {
+        return api.getAllCharacters()
+    }
+}
