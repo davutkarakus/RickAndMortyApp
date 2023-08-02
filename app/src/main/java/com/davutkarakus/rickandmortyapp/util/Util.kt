bb0bf1489ab4d906f1801aecd711527663f7dbec
Davut KARAKUS <pikachu_018@hotmail.com>
2023-08-02 14:04:49 +0300

    Editing isWifiEnabled func

--- app/src/main/java/com/davutkarakus/rickandmortyapp/util/Util.kt
+++ app/src/main/java/com/davutkarakus/rickandmortyapp/util/Util.kt
@@ -2,6 +2,8 @@ package com.davutkarakus.rickandmortyapp.util
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
@@ -49,9 +51,20 @@ fun stringAppend(t:TextView,episodes: List<String>?) {
 }
 
 fun isWifiEnabled(context: Context): Boolean {
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
-    return networkInfo?.isConnectedOrConnecting ?: false
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            }
+        }
+    return false
 }
 
 @BindingAdapter("android:onRefreshListener")
